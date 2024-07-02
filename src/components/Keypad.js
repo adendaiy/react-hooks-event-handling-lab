58237f3bea6dd9b1bb1b1d3a3c0545c428e19f46
adendaiy <adenabdib@gmail.com>
2024-07-02 13:54:48 +0100

    this is my react event handler lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from 'react';
+
+function Keypad() {
+  function handleChange(event) {
+    console.log('Entering password...');
+  }
+
+  return (
+    <div>
+      <label htmlFor="password">Enter password:</label>
+      <input type="password" id="password" name="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
+
+
